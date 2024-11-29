d0efb18743b5603a5cbf2cb1552c7accd53e05a8
Joaquin0505 <131311465+Joaquin0505@users.noreply.github.com>
2024-11-29 13:42:07 -0500

    ejercicio-mauro

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rds-ejerciciomauro.cjmo4gmwa7qa.us-west-1.rds.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+
   /* config options here */
 };
 
