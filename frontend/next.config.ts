f177e3f9e6aa921bf800d8e5554cf17b2197a34b
Joaquin0505 <131311465+Joaquin0505@users.noreply.github.com>
2024-11-29 14:14:55 -0500

    ejercicio-mauro

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  distDir: "dist",
+  reactStrictMode: true,
+  swcMinify: true,  
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "rds-ejerciciomauro.cjmo4gmwa7qa.us-west-1.rds.amazonaws.com",
-        port: "5432",
+        port: "",
         pathname: "/**",
       },
     ],
   },
-
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
